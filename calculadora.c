7f67330ceb84f7d7c49e4045300f424a7613320d
Miguel González <53835554+MiguelG52@users.noreply.github.com>
2022-09-07 13:12:31 -0500

    Potencia

--- calculadora.c
+++ calculadora.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
+double potencia(float num1, float potencia){
+  double resultado = (double)pow(num1, potencia);
+  return resultado; 
+}
 
 float suma(float num1, float num2)
 {
