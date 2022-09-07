09a5bf7a86e9484de7ee232d41cacc54fe86f5c8
FigueroaKevin <113006197+FigueroaKevin@users.noreply.github.com>
2022-09-07 13:28:56 -0500

    Create menu_calculadora.c

---
+++ menu_calculadora.c
@@ -0,0 +1,67 @@
+{
+
+int opcion,operador1,operador2;
+
+do
+{
+  
+  printf("1. Suma\n");
+  printf("2. Resta\n");
+  printf("3. Multiplicacion\n");
+  printf("4. Division\n");
+  printf("5. Raiz\n");
+  printf("6. Salir\n\n");
+
+  scanf("%d",&opcion);
+
+
+switch(opcion)
+{
+    case 1:
+        printf("Introduce un numero: ");
+        scanf("%d",&operador1);
+        printf("Introduce otro numero: ");
+        scanf("%d",&operador2);
+        //funcion suma
+        break;
+
+    case 2:
+        printf("Introduce un numero: ");
+        scanf("%d",&operador1);
+        printf("Introduce otro numero: ");
+        scanf("%d",&operador2);
+       //funcion resta
+        break;
+
+    case 3:
+        printf("Introduce un numero: ");
+        scanf("%d",&operador1);
+        printf("Introduce otro numero: ");
+        scanf("%d",&operador2);
+        //funcion multiplicacion
+        break;
+
+    case 4:
+        printf("Introduce un numero: ");
+        scanf("%d",&operador1);
+        printf("Introduce otro numero: ");
+        scanf("%d",&operador2);
+        //funcion divisio
+        break;
+     case 5:
+        printf("Introduce un numero: ");
+        scanf("%d",&operador1);
+        printf("Introduce otro numero: ");
+        scanf("%d",&operador2);
+        //funcion raiz
+        break;
+
+    case 5:
+      break;
+    
+        
+}
+
+}while(!(eleccion==6));
+
+}
